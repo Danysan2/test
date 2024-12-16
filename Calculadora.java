fb82652f0f10206678639ce81fc27e37bbf15073
Miguel Macias <lmmacias1986@gmail.com>
2024-12-16 16:45:32 -0500

    calculadora llamando multiplicacion y resta

--- Calculadora.java
+++ Calculadora.java
@@ -1,6 +1,19 @@
 public class Calculadora {
 
     public static void main(String[] args) {
+        System.out.println("Multiplicacion: " + multiplicacion());
+        System.out.println("Resta: " +  resta());
+    }
 
+    public static int multiplicacion(){
+        Producto multiplicacion = new Producto();
+        int resultado = multiplicacion.producto(3,5);
+        return resultado;
+    }
+
+    public static int resta(){
+        Resta resta = new Resta();
+        int resultado = resta.resta(3,5);
+        return resultado;
     }
 }
