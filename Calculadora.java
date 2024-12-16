baccb7d163d5c3d1524cca9721bfc8fd0cf06f58
Danysan2 <ingdanielcristancho@gmail.com>
2024-12-16 17:11:23 -0500

    mejorando el codigo

--- Calculadora.java
+++ Calculadora.java
@@ -3,21 +3,14 @@ public class Calculadora {
     public static void main(String[] args) {
 
         Suma.suma();
+        Resta.resta();
+        Producto.producto();
         Division.division();
-        System.out.println("Multiplicacion: " + multiplicacion());
-        System.out.println("Resta: " +  resta());
+
     }
 
 
-    public static int multiplicacion(){
-        Producto multiplicacion = new Producto();
-        int resultado = multiplicacion.producto(3,5);
-        return resultado;
-    }
 
-    public static int resta(){
-        Resta resta = new Resta();
-        int resultado = resta.resta(3,5);
-        return resultado;
-    }
+
+
 }
--- Producto.java
+++ Producto.java
@@ -1,5 +1,15 @@
+import java.util.Scanner;
+
 public class Producto {
-    public int producto(int number1 , int number2) {
-        return number1 * number2;
+    public static void producto(){
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Digite el primer numero");
+        int num1 = teclado.nextInt();
+        System.out.println("Digite el segundo numero");
+        int num2 = teclado.nextInt();
+
+        int resultado = num1 * num2;
+        System.out.println(resultado);
+
     }
 }
