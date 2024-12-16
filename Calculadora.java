3bbc9b9089b55274589343089e8c6b59a68e8f9d
Danysan2 <ingdanielcristancho@gmail.com>
2024-12-16 17:17:53 -0500

    mensaje de bienvenida

--- Calculadora.java
+++ Calculadora.java
@@ -1,7 +1,7 @@
 public class Calculadora {
 
     public static void main(String[] args) {
-
+        System.out.println("Bienvenido a la calculadora");
         Suma.suma();
         Resta.resta();
         Producto.producto();
