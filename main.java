9593b14f0dca5384c0586a7a0f96d0a1a7bbe3b6
Danysan2 <ingdanielcristancho@gmail.com>
2024-12-16 15:22:58 -0500

    Nuevo metodo

--- main.java
+++ main.java
@@ -1,5 +1,23 @@
-public class main {
+import java.util.Scanner;
+
+public class Main {
+
+
+    public static int suma(){
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Digite el primer numero");
+        int num1 = teclado.nextInt();
+        System.out.println("Digite el segundo numero");
+        int num2 = teclado.nextInt();
+        
+        return num1 + num2;
+
+
+
+
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hola mundo");
+       int resultado = suma();
     }
 }
